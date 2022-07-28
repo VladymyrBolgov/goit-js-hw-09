eb2a99878cdd74963b107698f646aaa09c06bc82
VladymyrBolgov <95247895+VladymyrBolgov@users.noreply.github.com>
2022-07-28 18:15:41 +0000

    Deploying to gh-pages from @ VladymyrBolgov/goit-js-hw-09@df678ef3b88f13b6f35b3e628cc890b91fe89660 🚀

--- 01-color-switcher.a32960f7.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]"),document.querySelector("button[data-stop]");console.log((function(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}));
-//# sourceMappingURL=01-color-switcher.a32960f7.js.map
