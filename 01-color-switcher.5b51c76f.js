3c12f056f4899d56dc51581039fbdd75a0d30d9d
VladymyrBolgov <95247895+VladymyrBolgov@users.noreply.github.com>
2022-07-28 19:00:40 +0000

    Deploying to gh-pages from @ VladymyrBolgov/goit-js-hw-09@c025fe2289401b214eb64f5dd13a2759a3af4bf1 🚀

---
+++ 01-color-switcher.5b51c76f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(o),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.5b51c76f.js.map
