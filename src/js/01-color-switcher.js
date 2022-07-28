c05610239c4dee0313af37b282f22df21f1861e4
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-28 21:38:51 +0300

    b

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,18 +3,22 @@ const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let timerId = null;
 
+startBtn.addEventListener('click', startChanging);
+stopBtn.addEventListener('click', stopChanging);
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-startBtn.addEventListener("click", () => {
+function startChanging() {
     timerId = setInterval(() => {
         body.style.backgroundColor = getRandomHexColor();
     }, 1000);
-  });
+    startBtn.setAttribute('(disabled', true);
+  };
   
   
-  stopBtn.addEventListener("click", () => {
+  function stopChanging() {
     clearInterval(timerId);
     console.log(`Interval with id ${timerId} has stopped!`);
-  });
+  };
